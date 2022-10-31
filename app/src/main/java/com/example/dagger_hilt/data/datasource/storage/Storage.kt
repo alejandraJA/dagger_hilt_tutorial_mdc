56bb667c24e8ccc3767d35ab9e7c44ba727b3f72
Alejandra Jimenez Avalos <55304400+alejandraJA@users.noreply.github.com>
2022-10-31 10:54:40 -0600

    Fix storage

--- app/src/main/java/com/example/dagger_hilt/data/datasource/storage/Storage.kt
+++ app/src/main/java/com/example/dagger_hilt/data/datasource/storage/Storage.kt
@@ -1,11 +1,17 @@
 package com.example.dagger_hilt.data.datasource.storage
 
+import android.content.Context
 import android.content.SharedPreferences
+import com.example.dagger_hilt.sys.util.Constants
+import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class Storage @Inject constructor(
-    private val sharedPreferences: SharedPreferences
+    @ApplicationContext private val app: Context
 ) {
+
+    private val sharedPreferences =
+        app.getSharedPreferences(Constants.USER_MEMORY, Context.MODE_PRIVATE)
 
     fun setString(key: String, value: String) {
         with(sharedPreferences.edit()) {
